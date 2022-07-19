2487d924bf123b153e34ee64e254d736d2188157
Marlene Guzman <41800372+mardb@users.noreply.github.com>
2022-07-19 13:54:03 -0700

    updating for debugging

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const { sequelize } = require('./models');
+// const { sequelize } = require('./models');
 
 var app = express();
 
